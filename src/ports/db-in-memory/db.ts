b414203e965354b81146d6eb5552bdf828df5d36
Marlon Menegatti <marlonmenegatt@gmail.com>
2022-06-04 13:58:53 -0300

    feat: add db in memory port and adapter

---
+++ src/ports/db-in-memory/db.ts
@@ -0,0 +1,9 @@
+
+import { OutsideRegisterType } from '@/adapters/use-cases/user/register-adapter'
+
+export const outsideRegister: OutsideRegisterType = async (data) => {
+  return {
+    success: true,
+    data,
+  }
+}
